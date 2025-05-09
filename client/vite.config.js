d1aefcf24576c395c2fa0ad78bdf890f8a2d86bb
Eslam Radwan <151574371+Eslam-Radwan@users.noreply.github.com>
2025-05-09 20:56:53 +0300

    Update vite.config.js

--- client/vite.config.js
+++ client/vite.config.js
@@ -4,4 +4,9 @@ import react from '@vitejs/plugin-react'
 // https://vite.dev/config/
 export default defineConfig({
   plugins: [react()],
+  base: '/', // or your preferred base path
+  build: {
+    outDir: 'dist',
+    emptyOutDir: true,
+  }
 })
